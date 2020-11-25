4bf483b083947f7cd2f9e3e54d8ffb1ec4bcee1e
Nikhil Shanbhag <61755381+Nikhil-1503@users.noreply.github.com>
2020-11-25 12:46:03 +0530

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -10,90 +10,62 @@ var slider = new Swiper(".swiper-container", {
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
-  }
+  },
+  on: {
+    slideChange: function() {
+      gsap.to(".slide-number span", 0.2, {
+        x: "-100px"
+      });
+      gsap.to(".slide-text span", 0.2, {
+        x: "-100px"
+      });
+    },
+
+    slideChangeTransitionEnd: function() {
+      gsap.to(".slide-number span", 0.2, {
+        x: 0,
+        delay: 0.1
+      });
+      gsap.to(".slide-number span", 0, {
+        x: "100px"
+      });
+    
+      gsap.to(".slide-text span", 0.2, {
+        x: 0
+      });
+      gsap.to(".slide-text span", 0, {
+        x: "100px"
+      });
+    
+      gsap.to(".swiper-slide-active", 0.5, {
+        scale: 1,
+        ease: Power4.easeOut
+      });
+    
+      gsap.to(".swiper-slide-active .slide-number", 0, {
+        autoAlpha: 1
+      });
+      gsap.to(".swiper-slide-active .slide-text", 0, {
+        autoAlpha: 1
+      });
+    
+      gsap.to(".swiper-slide-next .slide-number", 0, {
+        autoAlpha: 0
+      });
+      gsap.to(".swiper-slide-prev .slide-number", 0, {
+        autoAlpha: 0
+      });
+    
+      gsap.to(".swiper-slide-next .slide-text", 0, {
+        autoAlpha: 0
+      });
+      gsap.to(".swiper-slide-prev .slide-text", 0, {
+        autoAlpha: 0
+      });
+    }
+  },
 });
 
 
-slider.on("slideChange", function() {
-  gsap.to(".slide-text span", 0.2, {
-    x: "-100px"
-  });
-  gsap.to(".slide-number span", 0.2, {
-    x: "-100px"
-  });
-  gsap.to(".slide-detail span", 0.5, {
-    x: "-1000px"
-  });
-  gsap.to(".swiper-slide-active", 0.5, {
-    scale: 0.85
-  });
-});
 
-slider.on("slideChangeTransitionEnd", function() {
-  gsap.to(".slide-text span", 0.2, {
-    x: 0,
-    delay: 0.1
-  });
-  gsap.to(".slide-text span", 0, {
-    x: "100px"
-  });
 
-  gsap.to(".slide-number span", 0.2, {
-    x: 0
-  });
-  gsap.to(".slide-number span", 0, {
-    x: "100px"
-  });
-
-  gsap.to(".slide-detail span", 0.2, {
-    x: 0
-  });
-
-  gsap.to(".swiper-slide-active", 0.5, {
-    scale: 1,
-    ease: Power4.easeOut
-  });
-
-  gsap.to(".swiper-slide-active .slide-text", 0, {
-    autoAlpha: 1
-  });
-  gsap.to(".swiper-slide-active .slide-number", 0, {
-    autoAlpha: 1
-  });
-
-  gsap.to(".swiper-slide-next .slide-text", 0, {
-    autoAlpha: 0
-  });
-  gsap.to(".swiper-slide-prev .slide-text", 0, {
-    autoAlpha: 0
-  });
-
-  gsap.to(".swiper-slide-next .slide-number", 0, {
-    autoAlpha: 0
-  });
-  gsap.to(".swiper-slide-prev .slide-number", 0, {
-    autoAlpha: 0
-  });
-});
-
-gsap.to(".swiper-slide-next .slide-text", 0, {
-  autoAlpha: 0
-});
-gsap.to(".swiper-slide-prev .slide-text", 0, {
-  autoAlpha: 0
-});
-
-gsap.to(".swiper-slide-next .slide-number", 0, {
-  autoAlpha: 0
-});
-gsap.to(".swiper-slide-prev .slide-number", 0, {
-  autoAlpha: 0
-});
-
-gsap.to(".swiper-slide", 0, {
-  scale: 0.85
-});
-
-gsap.to(".swiper-slide-active", 0, {
-  scale: 1
-});
